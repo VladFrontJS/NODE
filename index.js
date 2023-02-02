ce3484dfa40b4b880c52189638fec8a1cea8bd77
Vlad <tonkonoh.vlad@gmail.com>
2023-02-02 10:44:38 +0200

    push github

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const TelegramApi = require('node-telegram-bot-api');
 const { gameOptions, againOptions } = require('./options');
 
 const token = '6182098561:AAGowfBSJC-Sj8zQtMIYiJrvoZISEVWGmE4';
-
 const bot = new TelegramApi(token, { polling: true });
 
 const chats = {};
@@ -11,7 +10,7 @@ const startGame = async chatId => {
     await bot.sendMessage(chatId, 'Now I will guess the number from 0 to 9, try to guess.');
     const randomNumber = Math.floor(Math.random() * 10);
     chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, 'Guess', gameOptions);
+    await bot.sendMessage(chatId, 'Guess number', gameOptions);
 };
 
 const start = () => {
@@ -44,9 +43,11 @@ const start = () => {
     bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
+
         if (data === '/again') {
             return startGame(chatId);
         }
+
         if (data === chats[chatId]) {
             return bot.sendMessage(chatId, `Congratulations, you guessed the number ${chats[chatId]}`, againOptions);
         } else {
